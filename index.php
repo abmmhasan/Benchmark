74c20c4d81d5da7a5940ca22e92c6d0bb125cf75
A. B. M. Mahmudul Hasan <abmmhasan@users.noreply.github.com>
2025-07-30 17:00:27 +0600

    wip

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ use AbmmHasan\Benchmark\BenchmarkRunner;
 //
 // 1) Define your configurations
 //
-$configUsers = new BenchmarkConfig(
+$config1 = new BenchmarkConfig(
     url:            'https://localhost.internal/json',
     method:         'GET',
     headers:        ['Accept' => 'application/json'],
@@ -23,7 +23,7 @@ $configUsers = new BenchmarkConfig(
     name:           'webrick'
 );
 
-$configOrders = new BenchmarkConfig(
+$config2 = new BenchmarkConfig(
     url:            'https://local.easy.com.bd/api/json',
     method:         'GET',
     headers:        ['Accept' => 'application/json'],
@@ -37,10 +37,24 @@ $configOrders = new BenchmarkConfig(
     name:           'laravel'
 );
 
+$config3 = new BenchmarkConfig(
+    url:            'https://localhost.internal/',
+    method:         'GET',
+    headers:        ['Accept' => 'application/json'],
+    body:           null,
+    expectedStatus: 200,
+    threads:        5,
+    count:          500,
+    piping:         'optimal',
+    timeout:        2,
+    enableHttp2:    true,
+    name:           'wr-home'
+);
+
 //
 // 2) Create the runner with as many configs as you like
 //
-$runner = new BenchmarkRunner($configUsers, $configOrders);
+$runner = new BenchmarkRunner($config1, $config2, $config3);
 //$runner = new BenchmarkRunner($configOrders);
 
 //
